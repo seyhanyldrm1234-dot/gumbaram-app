f81213d28763198e4638ce965ed451bddd487d31
seyhanyldrm1234-dot <seyhanyldrm1234@gmail.com>
2026-04-18 01:22:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'ZAMAN GELDİ! UNUTMADAN YATIRIMINI YAP',
-        icon: 'https://i.hizliresim.com/3wlhjf8.jpeg',
-        vibrate: [200, 100, 200],
-        tag: 'yatirim-bildirim', // Bildirimlerin üst üste binmesini engeller
-        renotify: true // Her seferinde sesi/titreşimi zorlar
-    };
-    event.waitUntil(self.registration.showNotification('GUMBARAM', options));
-});
-
-// iPhone'u uyanık tutmak için ekleme
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(clients.openWindow('/'));
+});
+
+// Bu kısım arka planda bildirim gelmesini zorlar
+self.addEventListener('message', function(event) {
+    if (event.data && event.data.type === 'SCHEDULE_NOTIFICATION') {
+        const options = {
+            body: 'ZAMAN GELDİ! UNUTMADAN YATIRIMINI YAP',
+            icon: 'https://i.hizliresim.com/3wlhjf8.jpeg',
+            badge: 'https://i.hizliresim.com/3wlhjf8.jpeg',
+            vibrate: [200, 100, 200],
+            tag: 'yatirim-vakti',
+            renotify: true
+        };
+
+        // 10 saniye sonra bildirim gönder
+        setTimeout(() => {
+            self.registration.showNotification('GUMBARAM', options);
+        }, 10000); 
+    }
 });
